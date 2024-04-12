f52a32e7b7b646d1a55d05867a3ad026443eb76c
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2024-04-12 21:20:58 +0100

    add show more button

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -16,7 +16,7 @@ export default function Search() {
 
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
-    
+    const [showMore, setShowMore] = useState(false);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -50,9 +50,13 @@ export default function Search() {
 
         const fetchListings = async () => {
             setLoading(true);
+            setShowMore(false);
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
+            if (data.length > 8){
+              setShowMore(true);
+            }
             setListings(data);
             setLoading(false);
         };
@@ -98,7 +102,21 @@ export default function Search() {
         urlParams.set('order', sidebardata.order);
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
+    };
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9){
+      setShowMore(false);
     }
+    setListings([...listings, ...data]); 
+  }
 
   return (
     <div className="flex flex-col md:flex-row">
@@ -211,6 +229,15 @@ export default function Search() {
                     )
                   )
                 }
+                {showMore && (
+                  <button
+                    onClick={() => {
+                      onShowMoreClick();
+                    }}
+                    className="text-green-700 hover:underline p-7 text-center w-full">
+                      Show more
+                    </button>
+                )}
             </div>
         </div>
     </div>
