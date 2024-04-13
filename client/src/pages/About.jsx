5be975aecc67b94316c2ff7d7760a4018f4127de
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2024-04-13 17:49:37 +0100

    about page done and title updated

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 
 export default function About(){
     return (
-        <div>About</div>
+        <div className='py-20 px-4 max-w-6xl mx-auto'>
+            <h1 className='text-3xl font-bold mb-4 text-teal-800'>About HOMEstate</h1>
+            <p className='mb-4 text-teal-700'>
+                Welcome to HOMEstate, where we believe every property is more than just bricks and mortar; it's a gateway to a lifetime of memories. At HOMEstate, we're passionate about connecting people with their dream homes, whether it's a cozy apartment in the heart of the city, a serene countryside retreat, or a luxurious beachfront villa.
+            </p>
+            <p className='mb-4 text-teal-700'>
+                Our mission is simple: to make the process of finding and owning a home as seamless and enjoyable as possible. With a commitment to personalized service and attention to detail, we strive to exceed our clients' expectations at every step of their journey towards homeownership.
+            </p>
+            <p className='mb-4 text-teal-700'>
+                What sets HOMEstate apart is our dedication to understanding our clients' unique needs and preferences. Whether you're a first-time buyer, seasoned investor, or looking to rent, our team of experienced professionals is here to guide you through every aspect of the real estate process.
+            </p>
+            <p className='mb-4 text-teal-700'>
+                But HOMEstate is more than just a real estate agency; it's a community. We're proud to foster a supportive and inclusive environment where our clients feel valued and empowered to make informed decisions about their future homes.
+            </p>
+            <p className='mb-4 text-teal-700'>
+                Thank you for considering HOMEstate for your real estate needs. We look forward to helping you find the perfect place to call home.
+            </p>
+
+        </div>
     )
 }
