45c7d68b57287639677ddbd68a2b8cf3a0f37187
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2024-04-18 00:26:48 +0100

    made changes to sign in and sign up

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -72,7 +72,7 @@ export default function SignUp() {
           onChange={handleChange}
         />
         <input 
-          type="text" 
+          type="password" 
           placeholder="password"
           className="border p-3 rounded-lg" 
           id="password" 
